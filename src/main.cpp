cf801bf18e177c438c4bf65eec9b5592e5facda4
Subhajit Sahu <wolfram77@gmail.com>
2016-09-01 22:01:45 +0530

    oswap: created and tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include "oswap.h"
 
 
 int main() {
+	int a = 0x1122;
+	int b = 0x3344;
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_VAR(a, b, int);
+	printf("OSWAP_VAR(a, b, int).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_BUF(a, b);
+	printf("OSWAP_BUF(a, b).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_ADDSUB(a, b);
+	printf("OSWAP_ADDSUB(a, b).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_XOR(a, b);
+	printf("OSWAP_XOR(a, b).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_BITSXOR(a, b, 0xFF00);
+	printf("OSWAP_BITSXOR(a, b, 0xFF00).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP(a, b);
+	printf("OSWAP(a, b).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
+
+	OSWAP_BITS(a, b, 0xFF00);
+	printf("OSWAP_BITS(a, b, 0xFF00).\n");
+	printf("a = 0x%X.\n", a);
+	printf("b = 0x%X.\n", b);
+	printf("\n");
 	return 0;
 }
---
+++ src/oswap.h
@@ -0,0 +1,84 @@
+#ifndef _OSWAP_H_
+#define _OSWAP_H_
+
+
+// Sanjaya said: At that time, King Duryodhana after seeing the army of the Pandavas arranged
+// in military phalanx approaching near his respected preceptor Dronacharya, spoke these words.
+// ref: http://www.bhagavad-gita.org/Gita/verse-01-02.html
+
+
+#pragma region required headers
+#include <string.h>
+#pragma endregion
+
+
+
+#pragma region macro
+// Swap variables using third variable.
+// args: varx, vary, type.
+#define OSWAP_VAR(X, Y, T) \
+	do { \
+		T z = X; \
+		X = Y; \
+		Y = z; \
+	} while(0)
+
+
+// Swap variables using temporary buffer.
+// args: varx, vary.
+// thanks:
+// adamk (http://github.com/OpenBudget)
+// http://stackoverflow.com/questions/3982348/implement-generic-swap-macro-in-c#answer-3982430
+#define OSWAP_BUF(X, Y) \
+	do { \
+		char z[sizeof(X)]; \
+		memcpy(z, &(X), sizeof(X)); \
+		memcpy(&(X), &(Y), sizeof(X)); \
+		memcpy(&(Y), z, sizeof(X)); \
+	} while(0)
+
+
+// Swap variables using addition and subtraction.
+// args: varx, vary.
+#define OSWAP_ADDSUB(X, Y) \
+	do { \
+		X = X + Y; \
+		Y = X - Y; \
+		X = X - Y; \
+	} while(0)
+
+
+// Swap variables using XOR.
+// args: varx, vary.
+#define OSWAP_XOR(X, Y) \
+	do { \
+		X = X ^ Y; \
+		Y = X ^ Y; \
+		X = X ^ Y; \
+	} while(0)
+
+
+// Swap individual bits using XOR.
+// args: varx, vary, bits.
+#define OSWAP_BITSXOR(X, Y, B) \
+	do { \
+		X = X ^ (Y & B); \
+		Y = Y ^ (X & B); \
+		X = X ^ (Y & B); \
+	} while(0)
+
+
+// Swap variables.
+// args: vara, varb.
+#define OSWAP(A, B) \
+	OSWAP_XOR(A, B)
+
+
+// Swap individual bits.
+// args: vara, varb, bits-value.
+#define OSWAP_BITS(A, B, V) \
+	OSWAP_BITSXOR(A, B, V)
+#pragma endregion
+
+
+#endif // !_OSWAP_H_
